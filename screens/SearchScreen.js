c4cc46d1e3a338700ac0cc3cfd49af00df633918
feelsuegood <suaj2021@gmail.com>
2023-06-08 19:46:05 +1000

    stock,search

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,37 +1,111 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, TouchableWithoutFeedback, Keyboard /* include other react native components here as needed */ } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TextInput,
+  FlatList,
+} from 'react-native';
 import { useStocksContext } from '../contexts/StocksContext';
 import { scaleSize } from '../constants/Layout';
 import { Ionicons } from '@expo/vector-icons';
 
-// FixMe: implement other components and functions used in SearchScreen here (don't just put all the JSX in SearchScreen below)
-
-
-
-
-
-
-
 export default function SearchScreen({ navigation }) {
   const { ServerURL, addToWatchlist } = useStocksContext();
-  const [state, setState] = useState({ /* FixMe: initial state here */ });
-
-  // can put more code here
+  const [state, setState] = useState({ query: '', results: [] });
 
   useEffect(() => {
     // FixMe: fetch symbol names from the server and save in local SearchScreen state
+    fetchSymbols();
   }, []);
+
+  const fetchSymbols = async () => {
+    try {
+      const response = await fetch(`${ServerURL}/symbols`);
+      const data = await response.json();
+      setState((prevState) => ({
+        ...prevState,
+        results: data.symbols,
+      }));
+    } catch (error) {
+      console.log('Error fetching symbols:', error);
+    }
+  };
+
+  const handleSearch = (text) => {
+    setState((prevState) => ({
+      ...prevState,
+      query: text,
+    }));
+  };
+
+  const handleSelectSymbol = (symbol) => {
+    addToWatchlist(symbol);
+    // FixMe: navigate to another screen or perform any other action after selecting a symbol
+  };
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={styles.container}>
-        {/* FixMe: add children here! */ }
+        <View style={styles.searchContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder='Search symbols...'
+            value={state.query}
+            onChangeText={handleSearch}
+          />
+          <Ionicons
+            name='ios-search'
+            size={24}
+            color='black'
+            style={styles.searchIcon}
+          />
+        </View>
+        <FlatList
+          data={state.results}
+          keyExtractor={(item) => item.symbol}
+          renderItem={({ item }) => (
+            <TouchableWithoutFeedback
+              onPress={() => handleSelectSymbol(item.symbol)}
+            >
+              <View style={styles.itemContainer}>
+                <Text>{item.symbol}</Text>
+                <Text>{item.name}</Text>
+              </View>
+            </TouchableWithoutFeedback>
+          )}
+        />
       </View>
-    </TouchableWithoutFeedback>    
-  )
+    </TouchableWithoutFeedback>
+  );
 }
 
 const styles = StyleSheet.create({
-// FixMe: add styles here ...
-// use scaleSize(x) to adjust sizes for small/large screens
+  container: {
+    flex: 1,
+    padding: scaleSize(16),
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: scaleSize(16),
+    paddingHorizontal: scaleSize(8),
+    backgroundColor: '#e0e0e0',
+    borderRadius: scaleSize(8),
+  },
+  input: {
+    flex: 1,
+    height: scaleSize(40),
+  },
+  searchIcon: {
+    paddingHorizontal: scaleSize(8),
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: scaleSize(8),
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
 });
