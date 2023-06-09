c38ff1506160903d09def7cde6f983f8fcdde157
feelsuegood <suaj2021@gmail.com>
2023-06-09 19:28:51 +1000

    just comments

--- services/fmpApi.js
+++ services/fmpApi.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const fmpApi = require('financialmodelingprep')(process.env.FMP_API_SECRET)
+const fmpApi = require('financialmodelingprep')(process.env.FMP_API_SECRET);
 // Official API documentation
 // if you need to use an endpoint that is not supported by the wrapper
 // use this to find the endpoints you need.
@@ -8,13 +8,20 @@ const fmpApi = require('financialmodelingprep')(process.env.FMP_API_SECRET)
 // Wrapper readme and example of fmpAPI.
 // https://github.com/patelneel55/financialmodelingprep/blob/master/README.md#api-documentation
 
-
 // Only use this when you need the news from stock symbols. the parameter is a list.
 const fmpNewsApi = (symbols) => {
-  const url = `https://financialmodelingprep.com/api/v3/stock_news?tickers=${symbols.join(',')}&page=0&apikey=${API_KEY}`;
+  const url = `https://financialmodelingprep.com/api/v3/stock_news?tickers=${symbols.join(
+    ','
+  )}&page=0&apikey=${API_KEY}`;
+  // ^ original code
+  // Change API_KEY to process.env.FMP_API_SECRET??
+  // const url = `https://financialmodelingprep.com/api/v3/stock_news?tickers=${symbols.join(
+  //   ','
+  // )}&page=0&apikey=${process.env.FMP_API_SECRET}`;
+
   return fetch(url)
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.error(err));
-}
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.error(err));
+};
 export default { fmpApi, fmpNewsApi };
