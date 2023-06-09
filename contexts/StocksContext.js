d4d08b5c65660e495c7affce214146dcbcd2fe8b
feelsuegood <suaj2021@gmail.com>
2023-06-09 14:45:42 +1000

    change navigation

--- contexts/StocksContext.js
+++ contexts/StocksContext.js
@@ -2,34 +2,62 @@ import React, { useState, useContext, useEffect } from 'react';
 import { AsyncStorage } from 'react-native';
 
 const StocksContext = React.createContext();
-//save stock information
 
 export const StocksProvider = ({ children }) => {
   const [state, setState] = useState([]);
-  // give stock info to children
+
+  useEffect(() => {
+    // Retrieve watchlist from persistent storage
+    // delete before submission
+    retrieveWatchlist();
+  }, []);
+
+  useEffect(() => {
+    // Save watchlist to AsyncStorage whenever it changes
+    saveWatchlist();
+  }, [state]);
+
+  const addToWatchlist = (newSymbol) => {
+    // Add the new symbol to the watchlist
+    //FixMe: add the new symbol to the watchlist, save it in useStockContext state and persist to AsyncStorage
+    // ^ Delete this comments before submission
+    const updatedWatchlist = [...state, newSymbol];
+    setState(updatedWatchlist);
+  };
+
+  const retrieveWatchlist = async () => {
+    try {
+      const storedWatchlist = await AsyncStorage.getItem('watchlist');
+      if (storedWatchlist !== null) {
+        setState(JSON.parse(storedWatchlist));
+      }
+    } catch (error) {
+      console.error('Error retrieving watchlist from AsyncStorage:', error);
+    }
+  };
+
+  const saveWatchlist = async () => {
+    try {
+      await AsyncStorage.setItem('watchlist', JSON.stringify(state));
+    } catch (error) {
+      console.error('Error saving watchlist to AsyncStorage:', error);
+    }
+  };
 
   return (
-    <StocksContext.Provider value={[state, setState]}>
+    <StocksContext.Provider value={[state, addToWatchlist]}>
       {children}
     </StocksContext.Provider>
   );
 };
 
 export const useStocksContext = () => {
-  const [state, setState] = useContext(StocksContext);
-  // get value of StocksContext and return state and setState
+  const [state, addToWatchlist] = useContext(StocksContext);
   // can put more code here
-
-  function addToWatchlist(newSymbol) {
-    //FixMe: add the new symbol to the watchlist, save it in useStockContext state and persist to AsyncStorage
-  }
-
-  useEffect(() => {
-    // FixMe: Retrieve watchlist from persistent storage
-  }, []);
+  // ^ Delete this comments before submission
 
   return {
-    ServerURL: 'http://131.181.190.87:3001',
+    ServerURL: '<http://131.181.190.87:3001>',
     watchList: state,
     addToWatchlist,
   };
