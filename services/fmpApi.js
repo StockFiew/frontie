58f2b05b6f6559fa1b653f92ee942c5174ce12e4
feelsuegood <suaj2021@gmail.com>
2023-06-09 19:56:09 +1000

    require('dotenv').config()  not supported in Expo

--- services/fmpApi.js
+++ services/fmpApi.js
@@ -1,5 +1,14 @@
-require('dotenv').config();
-const fmpApi = require('financialmodelingprep')(process.env.FMP_API_SECRET);
+import { FMP_API_SECRET } from '@env';
+import { FinancialModelingPrep } from 'financialmodelingprep';
+
+// require('dotenv').config();
+// const fmpApi = require('financialmodelingprep')(process.env.FMP_API_SECRET);
+// ^ original code
+// The require('dotenv').config() function is not supported in Expo, and using it will result in an error.
+
+const fmpApi = FinancialModelingPrep(FMP_API_SECRET);
+// const fmpApi = require('financialmodelingprep')(FMP_API_SECRET);
+// ^ original code
 // Official API documentation
 // if you need to use an endpoint that is not supported by the wrapper
 // use this to find the endpoints you need.
@@ -9,15 +18,18 @@ const fmpApi = require('financialmodelingprep')(process.env.FMP_API_SECRET);
 // https://github.com/patelneel55/financialmodelingprep/blob/master/README.md#api-documentation
 
 // Only use this when you need the news from stock symbols. the parameter is a list.
+
+// const fmpNewsApi = (symbols) => {
+//   const url = `https://financialmodelingprep.com/api/v3/stock_news?tickers=${symbols.join(
+//     ','
+//   )}&page=0&apikey=${API_KEY}`;
+// ^ original code
+// Change API_KEY to process.env.FMP_API_SECRET??
+
 const fmpNewsApi = (symbols) => {
   const url = `https://financialmodelingprep.com/api/v3/stock_news?tickers=${symbols.join(
     ','
-  )}&page=0&apikey=${API_KEY}`;
-  // ^ original code
-  // Change API_KEY to process.env.FMP_API_SECRET??
-  // const url = `https://financialmodelingprep.com/api/v3/stock_news?tickers=${symbols.join(
-  //   ','
-  // )}&page=0&apikey=${process.env.FMP_API_SECRET}`;
+  )}&page=0&apikey=${FMP_API_SECRET}`;
 
   return fetch(url)
     .then((res) => res.json())
